2443af45b3fd68f357510993d3993081e02215cd
Splendorr <simolinic@gmail.com>
2011-05-05 22:49:46 -0400

    Correct :title: to :page_title: to match convention

--- soups/dynasnips/title-for.snip.rb
+++ soups/dynasnips/title-for.snip.rb
@@ -1,8 +1,8 @@
 class TitleFor < Dynasnip
   def handle(snip_name=nil, tag="h1")
     snip = snip_name ? app.soup[snip_name] : app.request.snip
-    if snip.title
-      %{<#{tag}>#{snip.title}</#{tag}>}
+    if snip.page_title
+      %{<#{tag}>#{snip.page_title}</#{tag}>}
     end
   end
   self
