e1b1fdf57d55e35bc76e3648664e47bd615cdce3
Splendorr <simolinic@gmail.com>
2015-05-09 18:57:55 -0400

    Adding .to_s at least made something happen in the index, even if it's way wrong!

--- soups/awn/newindex.snip.rb
+++ soups/awn/newindex.snip.rb
@@ -1,6 +1,8 @@
 class Newindex < Dynasnip
   def get(*args)
-    list = app.soup.instance_eval { @backend }.send(:all_snips).sort_by { |a| a.updated_at || Time.at(0) }.reverse.map { |snip|
+    # list = app.soup.instance_eval { @backend }.send(:all_snips).sort_by { |a| a.updated_at || Time.at(0) }.reverse.map { |snip|
+    list = app.soup.instance_eval { @backend }.send(:all_snips).sort_by { |a| a.updated_at.to_s || Time.at(0) }.reverse.map { |snip|
+    # list = app.soup.instance_eval { @backend }.send(:all_snips).sort_by { |a| a.updated_at.to_s}.reverse.map { |snip|
       "#{link_to snip.page_title, snip.name}<br />"
     }
     "#{list}"
